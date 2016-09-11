d0bd21ad58435a7592947397b0032dfb2665d285
piberger <pberger@phys.ethz.ch>
2016-09-11 17:30:45 +0200

    reduce and improve debug output

--- ModulePositionProvider/LocalData.py
+++ ModulePositionProvider/LocalData.py
@@ -17,13 +17,20 @@ class ModulePositionProvider(AbstractModulePositionProvider):
         modulesListFileName = os.path.join(*pathParts)
 
         modulePosition = []
+        moduleFound = False
         if os.path.isfile(modulesListFileName):
             with open(modulesListFileName, 'r') as modulesListFile:
                 for line in modulesListFile:
-                    print ModuleID, line
                     lineSplitted = [x for x in line.replace('\t', ' ').split(' ') if len(x) > 0]
                     if lineSplitted[0] == ModuleID:
                         modulePosition = lineSplitted[1:]
+                        moduleFound = True
 
         modulePosition = [x.strip() for x in modulePosition if len(x.strip()) > 0]
+        if moduleFound:
+            print " -> module found:", ModuleID
+            print " -> position: ", '/'.join(modulePosition)
+        else:
+            print " -> module not found:", ModuleID
+
         return modulePosition
