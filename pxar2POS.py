a3de65bb32f13a3ccba51c1ea4b3b09469f389ff
piberger <pberger@phys.ethz.ch>
2016-09-11 15:59:02 +0200

    additional command line options for output folder, db url

--- pxar2POS.py
+++ pxar2POS.py
@@ -2,20 +2,10 @@
 import argparse
 from pxar2POSConverter import pxar2POSConverter as pxar2POSConverter
 
-# initialize converter
-options = {
-    'ModulePositionTable': 'ExampleData/modules.txt',
-    'DataSource': 'http://cmspixelprod.pi.infn.it',  # if it contains http://, it will be interpreted as url to sql server, otherwise as a local path
-    'OutputPath': 'OutputData'
-}
-print "-"*80
-print " initialize pxar -> POS converter:"
-print "-"*80
-print "  -> module positions from ", options['ModulePositionTable']
-print "  -> module data from ", options['DataSource']
-print "  -> save data in ", options['OutputPath']
 
-converter = pxar2POSConverter(options=options)
+print "-"*80
+print " pxar -> POS converter:"
+print "-"*80
 
 # read arguments from command line
 parser = argparse.ArgumentParser(description='converter for pxar data from database to pixel online software')
@@ -25,8 +15,38 @@ parser.add_argument('-m','--module',dest='module',
 parser.add_argument('-T', '--trim', dest='trim',
                     help='trim value, e.g. 35',
                     default='35')
+parser.add_argument('-t', '--temp', dest='temp',
+                    help='temperature',
+                    default='-20')
+parser.add_argument('-o', '--output', dest='output',
+                    help='output folder',
+                    default='OutputData')
+parser.add_argument('-p', '--positions', dest='positions',
+                    help='module positions file',
+                    default='ExampleData/modules.txt')
+parser.add_argument('-u', '--url', dest='url',
+                    help='url to database',
+                    default='http://cmspixelprod.pi.infn.it')
+
+
+
 args = parser.parse_args()
 
+print "  -> module:", args.module
+print "  -> trim:", args.trim
+print "  -> temperature:", args.temp
+print "  -> module positions from:", args.positions
+print "  -> module data from:", args.url
+print "  -> save data in:", args.output
+
+# initialize converter
+print "initialize converter..."
+options = {
+    'ModulePositionTable': args.positions,
+    'DataSource': args.url,  # if it contains http://, it will be interpreted as url to sql server, otherwise as a local path
+    'OutputPath': args.output
+}
+converter = pxar2POSConverter(options=options)
 
 # select which Fulltest of FullQualification to use
 testOptions = {'Test': '*ulltest*_m20', 'TrimValue': args.trim}
