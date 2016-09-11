dab29f29d36cf38fc9fe0b63d77c676922121002
piberger <pberger@phys.ethz.ch>
2016-09-11 17:27:55 +0200

    also write DACs/trims/TBMpars, if one of the others could not be read and produces and exception (but show warning in the end)

--- pxar2PosConverter.py
+++ pxar2PosConverter.py
@@ -33,21 +33,44 @@ class pxar2POSConverter(object):
         #  -> ['BPix', 'BmO', 'SEC1', 'LYR2', 'LDR1H', 'MOD4']
         modulePosition = self.modulePositionTable.getModulePosition(moduleID)
 
-        print "read data for module {moduleID}...".format(moduleID=moduleID)
+        print "read/write data for module {moduleID}...".format(moduleID=moduleID)
+        errorsOccurred = 0
 
-        # read DAC parameters
-        rocDACs = self.dataSource.getRocDacs(ModuleID=moduleID, options=testOptions)
+        try:
+            # read DAC parameters
+            rocDACs = self.dataSource.getRocDacs(ModuleID=moduleID, options=testOptions)
 
-        # read trimbits
-        rocTrimbits = self.dataSource.getTrimBits(ModuleID=moduleID, options=testOptions)
+            # write DAC parameters
+            self.posWriter.writeDACs(moduleID, modulePosition, rocDACs)
+        except Exception as e:
+            print e
+            print "ERROR: could not read/write DAC parameters"
+            errorsOccurred += 1
 
-        # read TBM parameters
-        tbmParameters = self.dataSource.getTbmParameters(ModuleID=moduleID, options=testOptions)
+        try:
+            # read trimbits
+            rocTrimbits = self.dataSource.getTrimBits(ModuleID=moduleID, options=testOptions)
+
+            # write trimbits
+            self.posWriter.writeTrim(moduleID, modulePosition, rocTrimbits)
+        except Exception as e:
+            print e
+            print "ERROR: could not read/write trimbits"
+            errorsOccurred += 1
+
+        try:
+            # read TBM parameters
+            tbmParameters = self.dataSource.getTbmParameters(ModuleID=moduleID, options=testOptions)
+
+            # write TBM parameters to pixel online format
+            self.posWriter.writeTBM(moduleID, modulePosition, tbmParameters)
+        except Exception as e:
+            print e
+            print "ERROR: could not read/write TBM parameters"
+            errorsOccurred += 1
 
 
-        print "write data for module {moduleID}...".format(moduleID=moduleID)
-
-        # write to pixel online format
-        self.posWriter.writeDACs(moduleID, modulePosition, rocDACs)
-        self.posWriter.writeTrim(moduleID, modulePosition, rocTrimbits)
-        self.posWriter.writeTBM(moduleID, modulePosition, tbmParameters)
+        if errorsOccurred < 1:
+            print " --> done."
+        else:
+            print " --> done, but some errors occurred!!!"
